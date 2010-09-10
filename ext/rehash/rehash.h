8ec21e918a7bb14238057763637839343b6c3965
Lourens Naudé <lourens@methodmissing.com>
2010-09-10 01:29:23 +0100

    Implement Hash#revalue!

---
+++ ext/rehash/rehash.h
@@ -0,0 +1,19 @@
+typedef struct {
+  st_table *tbl;
+  VALUE method;
+} rehash_arg;
+
+static ID id_rekey_bang, id_revalue_bang;
+
+#define HASH_REHASH_METHOD(tbl, k, v) \
+  if ((k) != Qundef) st_insert(tbl, (k), (v)); \
+  return ST_CONTINUE;
+
+#define HASH_REHASH_WITH(hash, method, func) \
+    rehash_arg arg; \
+    HASH_MOD_CHECK(hash) \
+    arg.method = method; \
+    arg.tbl = st_init_table_with_size(HASH_TYPE(hash), HASH_SIZE(hash)); \
+    rb_hash_foreach(hash, func, (rehash_arg*)&arg); \
+    st_free_table(HASH_TBL(hash)); \
+    HASH_TBL(hash) = arg.tbl;
--- test/test_rehash.rb
+++ test/test_rehash.rb
@@ -7,7 +7,19 @@ class TestRehash < Test::Unit::TestCase
     assert_rekeyed hash, [:A, :B]
   end
 
+  def test_revalue_bang
+    hash = { 1 => 'a', 2 => 'b' }
+    hash.revalue!(:upcase)
+    assert_revalued hash, %w(A B)
+    hash.revalue!(:to_sym)
+    assert_revalued hash, [:A, :B]
+  end
+
   private
+  def assert_revalued(hash, expected)
+    assert_rehased(hash, :value?, expected)
+  end
+
   def assert_rekeyed(hash, expected)
     assert_rehased(hash, :key?, expected)
   end
