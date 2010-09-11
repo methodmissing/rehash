be56b90a7d0fc664d7552a0d5aef5fe830b6d569
Lourens Naudé <lourens@methodmissing.com>
2010-09-11 01:52:16 +0100

    Lookup method prior to firing off the rehash loop

--- ext/rehash/rehash.c
+++ ext/rehash/rehash.c
@@ -9,34 +9,28 @@
 
 #include <rehash.h>
 
-static int
+static inline int
 rb_hash_rekey_i(VALUE key, VALUE value, rehash_arg* arg)
 {
-    ID method;
-    method = rb_to_id(arg->method);
-    HASH_REHASH_METHOD(arg->tbl, rb_funcall(key, method, 0, 0), value)
+    HASH_REHASH_METHOD(arg->tbl, rb_funcall(key, arg->method, 0, 0), value)
 }
 
 static VALUE
 rb_hash_rekey_bang(VALUE hash, VALUE method)
 {
     HASH_REHASH_WITH(hash, method, rb_hash_rekey_i)
-    return hash;
 }
 
-static int
+static inline int
 rb_hash_revalue_i(VALUE key, VALUE value, rehash_arg* arg)
 {
-    ID method;
-    method = rb_to_id(arg->method);
-    HASH_REHASH_METHOD(arg->tbl, key, rb_funcall(value, method, 0, 0))
+    HASH_REHASH_METHOD(arg->tbl, key, rb_funcall(value, arg->method, 0, 0))
 }
 
 static VALUE
 rb_hash_revalue_bang(VALUE hash, VALUE method)
 {
     HASH_REHASH_WITH(hash, method, rb_hash_revalue_i)
-    return hash;
 }
 
 void
--- ext/rehash/rehash.h
+++ ext/rehash/rehash.h
@@ -1,6 +1,6 @@
 typedef struct {
   st_table *tbl;
-  VALUE method;
+  ID method;
 } rehash_arg;
 
 static ID id_rekey_bang, id_revalue_bang;
@@ -12,8 +12,9 @@ static ID id_rekey_bang, id_revalue_bang;
 #define HASH_REHASH_WITH(hash, method, func) \
     rehash_arg arg; \
     HASH_MOD_CHECK(hash) \
-    arg.method = method; \
+    arg.method = rb_to_id(method); \
     arg.tbl = st_init_table_with_size(HASH_TYPE(hash), HASH_SIZE(hash)); \
     rb_hash_foreach(hash, func, (rehash_arg*)&arg); \
     st_free_table(HASH_TBL(hash)); \
-    HASH_TBL(hash) = arg.tbl;
+    HASH_TBL(hash) = arg.tbl; \
+    return hash;
